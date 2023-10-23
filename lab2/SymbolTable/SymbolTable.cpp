c30e3242363b15da256157148e6736d9bf46b06b
Topan Oana <oanamariat1@gmail.com>
2023-10-23 21:55:41 +0300

    mici modificari la position function

--- lab2/SymbolTable/SymbolTable.cpp
+++ lab2/SymbolTable/SymbolTable.cpp
@@ -1,5 +1,22 @@
+//symboltable.cpp
 //
 // Created by oanam on 10/23/2023.
 //
 
 #include "SymbolTable.h"
+
+SymbolTable::SymbolTable() {
+    hashTable = *new HashTable<string>();
+}
+
+int SymbolTable::position(string token) {
+    return hashTable.position(token);
+}
+
+int SymbolTable::removeToken(string token) {
+    return hashTable.removeValue(token);
+}
+
+int SymbolTable::addToken(string token) {
+    return hashTable.addValue(token);
+}
--- lab2/SymbolTable/SymbolTable.h
+++ lab2/SymbolTable/SymbolTable.h
@@ -5,8 +5,31 @@
 #ifndef LAB2_SYMBOLTABLE_H
 #define LAB2_SYMBOLTABLE_H
 
+#include <string>
+#include <vector>
+#include "../HashTable/HashTable.h"
+
+
+using namespace std;
+
 
 class SymbolTable {
+private:
+    HashTable<string> hashTable;
+
+public:
+
+    SymbolTable();
+
+
+    int position(string token);
+
+    int removeToken(string token);
+
+    int addToken(string token);
+
+
+
 
 };
 
